2be75d9f6701765300e4c6469252d2928e472e8e
manlowtech <charumbiramanlow20@gmail.com>
2021-08-07 21:56:43 +0200

    boilerPlate

---
+++ bin/create_mernpress.js
@@ -0,0 +1,38 @@
+const {execSync} = require('child_process')
+const path = require('path')
+const fs = require('fs')
+
+if(ProcessingInstruction.argv.length < 3){
+    console.log("you have to provide name for your project")
+    console.log("For Example")
+    console.log("npx create-mernpress app-name")
+    process.exit(1)
+}
+
+const projectName = process.argv[2];
+const cPath = process.cwd()
+const ProjectPath = path.join(cPath,ProjectPath)
+const git_repo = "http://github.com/manlowtech/mernpress"
+
+try{
+    fs.mkdirSync(ProjectPath)
+}catch(err){
+    if(err.code == 'EEEXIST'){
+        console.log("Provided name already exist")
+    }
+    process.exit(1)
+}
+
+async function main() {
+    try{
+      console.log("Downloading File ...")
+      execSync(`git clone --depth=1 ${git_repo} ${ProjectPath}`)
+      process.chdir(ProjectPath)
+      console.log("Installing Dependencies")
+      execSync('npm install')
+      console.log("The Installation is done , npm start to use")
+    }catch(err){
+        console.log(err)
+    }
+}
+main()
